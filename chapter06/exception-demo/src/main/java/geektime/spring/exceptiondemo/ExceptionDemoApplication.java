28f3ff799e2882b551e45e2b17af704a9df292ec
王宁 <wangningc@foxmail.com>
2020-02-11 10:07:08 +0800

    feat(exception demo): add exception demo

---
+++ chapter06/exception-demo/src/main/java/geektime/spring/exceptiondemo/ExceptionDemoApplication.java
@@ -0,0 +1,23 @@
+package geektime.spring.exceptiondemo;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableCaching
+public class ExceptionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionDemoApplication.class, args);
+	}
+
+	@Bean
+	public Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
+	}
+}
---
+++ chapter06/exception-demo/src/main/java/geektime/spring/exceptiondemo/model/OrderState.java
@@ -0,0 +1,5 @@
+package geektime.spring.exceptiondemo.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
